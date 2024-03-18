15ee0a2ee5b258fffa3d0e9f3f168c434501370b
aboluo2003 <316266181@qq.com>
2024-03-18 13:46:27 +0800

    Site updated: 2024-03-18 13:46:26

--- js/hidden-text.js
+++ js/hidden-text.js
@@ -1,4 +1,4 @@
-var hidden_text_list = $('.text-hidden');
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var hidden_text_list = $('.text-hidden');
 for (var i = 0; i < hidden_text_list.length; i++) {
   x = hidden_text_list[i];
   x.title = '你知道的太多了';
